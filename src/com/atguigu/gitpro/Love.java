3f943530b1811a618389b427cfd63b063fb87527
liuxuewen5599 <liuxuewen5599@163.com>
2020-02-25 21:09:28 +0800

    懒得想故事，就这样吧

--- src/com/atguigu/gitpro/Love.java
+++ src/com/atguigu/gitpro/Love.java
@@ -3,7 +3,7 @@ package com.atguigu.gitpro;
 public class Love {
 
 	public static void main(String[] args) {
-		System.out.println("��������£���������");
+		System.out.println("懒得想故事，就这样吧");
 		System.out.println("Who are you ?");
 		System.out.println("Are you a handsome boy????");
 	}
